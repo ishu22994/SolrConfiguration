336c76f743f2e2c770edd78ab9ee60afad3f4e3c
samika98 <samikakashyap@gmail.com>
2020-01-24 20:31:43 +0530

    Search Done

--- src/main/java/com/example/demoSpringSolr/DemoSpringSolrApplication.java
+++ src/main/java/com/example/demoSpringSolr/DemoSpringSolrApplication.java
@@ -2,8 +2,10 @@ package com.example.demoSpringSolr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DemoSpringSolrApplication {
 
 	public static void main(String[] args) {
